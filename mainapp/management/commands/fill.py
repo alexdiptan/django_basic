aa9bad6d19288c92e759fe21e525e98fd3e28b4c
aleksandrdiptan <diptan@huntflow.ru>
2022-05-28 06:50:54 +0300

    - added fill.py command file (to load data in db). Also added json files with data to fill db tables products.json and categories.json - added new add - auth app

---
+++ mainapp/management/commands/fill.py
@@ -0,0 +1,39 @@
+import json
+
+from django.core.management import BaseCommand
+from django.conf import settings
+from mainapp.models import Category, Product
+
+
+class Command(BaseCommand):
+    @staticmethod
+    def _load_data_from_file(file_name):
+        with open(f'{settings.BASE_DIR}/mainapp/json/{file_name}.json') as json_file:
+            return json.load(json_file)
+
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
+
+        categories_list = self._load_data_from_file('categories')
+
+        categories_batch = []
+        for cat in categories_list:
+            categories_batch.append(
+                Category(
+                    name=cat.get('name'),
+                    description=cat.get('description')
+                )
+            )
+
+        Category.objects.bulk_create(categories_batch)
+
+        Product.objects.all().delete()
+
+        products_list = self._load_data_from_file('products')
+
+        for prod in products_list:
+            _cat = Category.objects.filter(name=prod.get('category')).first()
+            print(_cat)
+            prod['category'] = _cat
+
+            Product.objects.create(**prod)
