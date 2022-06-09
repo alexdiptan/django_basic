2fd2fae1016637fd066de753e2c3daf7f0c65fcb
aleksandrdiptan <diptan@huntflow.ru>
2022-06-09 18:08:02 +0300

    added: - product create - product list - category read

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,6 +7,17 @@ app_name = 'adminapp'
 urlpatterns = [
     path('users/create/', adminapp.user_create, name='user_create'),
     path('users/read/', adminapp.user_read, name='user_read'),
-    path('users/update/<int:pk>', adminapp.user_update, name='user_update'),
-    path('users/delete/<int:pk>', adminapp.user_delete, name='user_delete'),
+    path('users/update/<pk>/', adminapp.user_update, name='user_update'),
+    path('users/delete/<pk>/', adminapp.user_delete, name='user_delete'),
+
+    path('categories/create/', adminapp.category_create, name='category_create'),
+    path('categories/read/', adminapp.category_read, name='category_read'),
+    path('categories/update/<pk>/', adminapp.category_update, name='category_update'),
+    path('categories/delete/<pk>/', adminapp.category_delete, name='category_delete'),
+    path('categories/products/list/<pk>/', adminapp.products_list, name='products_list'),
+
+    path('products/create/<pk>/', adminapp.product_create, name='product_create'),
+    path('products/update/<pk>/', adminapp.product_update, name='product_update'),
+    path('products/delete/<pk>/', adminapp.product_delete, name='product_delete'),
+    path('products/detail/<pk>/', adminapp.product_detail, name='product_detail'),
 ]
